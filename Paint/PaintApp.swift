a038a3f0f21e150dc3b17cb3031cc026fcca795f
guyardito <guyardito@gmail.com>
2023-01-21 16:15:45 -0500

    Initial Commit

---
+++ Paint/PaintApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaintApp.swift
+//  Paint
+//
+//  Created by Dev on 1/21/23.
+//
+
+import SwiftUI
+
+@main
+struct PaintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
